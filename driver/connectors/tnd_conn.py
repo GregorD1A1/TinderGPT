5e81f5accac910f1e1b56cabda99f4ca9f21a3e6
GregorD1A1 <dudnikgrv@gmail.com>
2024-06-03 17:45:43 +0200

    opener looking for only prevented

--- driver/connectors/tnd_conn.py
+++ driver/connectors/tnd_conn.py
@@ -144,7 +144,7 @@ class TinderConnector():
         except NoSuchElementException:
             bio = self.driver.find_element('xpath', self.unwritten_girl_full_bio_xpath).text
         else:
-            if len(bio) < 25:
+            if len(bio) < 25 or bio.startswith('Looking for'):
                 bio = self.driver.find_element('xpath', self.unwritten_girl_full_bio_xpath).text
 
         return name, bio
